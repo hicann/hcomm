b603a69abf0ad559289267e366d054aacbfca9c1
fanbin1004 <fanbin37@h-partners.com>
2026-06-12 17:03:03 +0800

    [fix]管理单例类的实例化，确保销毁顺序正确
    Co-authored-by: fwx1313642<fanbin37@h-partners.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !2641 merge 9.1.0-beta.2 into 9.1.0-beta.2
    
    [fix]管理单例类的实例化，确保销毁顺序正确
    
    Created-by: fanbin1004
    Commit-by: fwx1313642
    Merged-by: cann-robot
    Description: ## 描述
    <!--在这里详细描述你的改动，包括改动的原因和所采取的方法。-->
    单例类的销毁顺序不保序，导致用例退出时有error日志打印
    
    ## 变更类型
    请选择本次引入的变更类型：
    <!--  [x] 表示选中 -->
    - [x] 🐛 Bug修复
    - [ ] ✨ 新特性
    - [ ] 🚀 性能优化
    - [ ] 📝 文档更新
    - [ ] 📋 其他，请描述：
    
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。-->
    <!-- 如果这个PR不涉及Issue，可填写"NA"。-->
    NA
    
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。包括但不限于构造对应xx测试用例、二级冒烟、算子泛化等。-->
    已完成的测试用例和场景：
    1. 测试验证，ut/st
    2. 冒烟
    
    补充的UT用例：
    NA
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    NA
    ## 合入检查
    <!-- 在正式合入前，请做好必要的代码测试，用例补充，软件代码风格检查等。提高合入效率。-->
    <!--  [x] 表示选中 -->
    - [x] 🧐 已经详细阅读了贡献指南（CONTRIBUTING.md），并遵守了其中的所有规定，包括但不限于commit message的格式、无效commit的合并等
    - [x] 🔍 邀请 committer评论`/lgtm`前的必要检查
        - [ ] 🏷️ 标题中使用了合适的类型标签（如：`[feat]`, `[fix]`）
        - [ ] 📄 代码修改内容已简要描述，相关文档已更新
        - [ ] 📝 代码注释已更新，代码遵循项目整体代码风格
        - [ ] 🧪 代码UT测试已更新，覆盖率已达标
        - [ ] 🔬 验证方法已更新到"测试"部分
        - [ ] 🛠️ 代码已通过静态分析工具检查，无错误
        - [ ] 👥 代码检视/code review/同行评议和必要的代码串讲，确保代码质量
        - [ ] ✅ 代码检视意见已处理或答复，无未处理的检视意见
    - [x] 🚀 预约 前冒烟 用例前的必要检查
        - [ ] ✔️ 代码已有committer的`/lgtm` 和 模块committer的`/lgtm`评论
        - [ ] 🔧 代码已通过compile，编译无错误，无告警
        - [ ] 🖥️ 代码已通过基本功能本地测试或者在线测试，确保基本功能正常
    - [x] 🎯 预约 approver评论`/approve`，正式合入前的必要检查
        - [ ] 📊 前冒烟 用例已全量通过
        - [ ] 📦 新增功能已同步补充基本功能测试用例到前冒烟里
    
    See merge request: cann/hcomm!2641

--- src/framework/next/comms/api_c_adpt/hcomm_c_adpt.h
+++ src/framework/next/comms/api_c_adpt/hcomm_c_adpt.h
@@ -64,7 +64,7 @@ typedef struct {
     };
 } HcommEndpointListenConfig;
 
-HcommResult HcommResMgrInit(uint32_t devPhyId);
+HcommResult HcommResMgrInit(uint32_t devPhyId = UINT32_MAX);
 
 HcommResult HcommEndpointGet(EndpointHandle endpointHandle, void **endpoint);
 
