a5b8d371c39db07262c479d724447a8bb97c9080
qinhan123 <qinhan10@huawei.com>
2026-09-10 14:25:33 +0800

    hcomm安装权限规范统一
    Co-authored-by: q00889097<qinhan10@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !5150 merge 9.2.0 into 9.2.0
    
    hcomm安装权限规范统一
    
    Created-by: qinhan123
    Commit-by: q00889097
    Merged-by: cann-robot
    Description: ## 描述
    <!--在这里详细描述你的改动，包括改动的原因和所采取的方法。-->
    该 PR 旨在统一 hcomm 安装包的权限规范：移除 hcomm 安装与打包流程中与公共规范不一致的特殊权限设置和冗余打包内容，使安装目录与文件权限遵循统一标准。主要涉及删除 cmake/package.cmake 中 path.cfg 配置文件的打包安装、移除 hcomm.xml 中的 EngineeringFiles 块，并清理 install.sh 与 hcomm_func.sh 中针对 python、hccl、version.info、ascend_install.info 及 compiler 目录等特殊 chmod 逻辑。
    ## 变更类型
    请选择本次引入的变更类型：
    <!--  [x] 表示选中 -->
    - [ ] 🐛 Bug修复
    - [x] ✨ 新特性
    - [ ] 🚀 性能优化
    - [ ] 📝 文档更新
    - [ ] 📋 其他，请描述：
    
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。-->
    <!-- 如果这个PR不涉及Issue，可填写"NA"。-->
    
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。包括但不限于构造对应xx测试用例、二级冒烟、算子泛化等。-->
    已完成的测试用例和场景：
    1. 
    2. 
    
    补充的UT用例：
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 合入检查
    <!-- 在正式合入前，请做好必要的代码测试，用例补充，软件代码风格检查等。提高合入效率。-->
    <!--  [x] 表示选中 -->
    - [x] 🧐 已经详细阅读了贡献指南（CONTRIBUTING.md），并遵守了其中的所有规定，包括但不限于commit message的格式、无效commit的合并等
    - [ ] 🔍 邀请 committer评论`/lgtm`前的必要检查
        - [ ] 🏷️ 标题中使用了合适的类型标签（如：`[feat]`, `[fix]`）
        - [ ] 📄 代码修改内容已简要描述，相关文档已更新
        - [ ] 📝 代码注释已更新，代码遵循项目整体代码风格
        - [ ] 🧪 代码UT测试已更新，覆盖率已达标
        - [ ] 🔬 验证方法已更新到"测试"部分
        - [ ] 🛠️ 代码已通过静态分析工具检查，无错误
        - [ ] 👥 代码检视/code review/同行评议和必要的代码串讲，确保代码质量
        - [ ] ✅ 代码检视意见已处理或答复，无未处理的检视意见
    - [ ] 🚀 预约 前冒烟 用例前的必要检查
        - [ ] ✔️ 代码已有committer的`/lgtm` 和 模块committer的`/lgtm`评论
        - [ ] 🔧 代码已通过compile，编译无错误，无告警
        - [ ] 🖥️ 代码已通过基本功能本地测试或者在线测试，确保基本功能正常
    - [ ] 🎯 预约 approver评论`/approve`，正式合入前的必要检查
        - [ ] 📊 前冒烟 用例已全量通过
        - [ ] 📦 新增功能已同步补充基本功能测试用例到前冒烟里
    
    See merge request: cann/hcomm!5150

--- cmake/fetch_cann_cmake.cmake
+++ cmake/fetch_cann_cmake.cmake
@@ -14,12 +14,12 @@ if(NOT PROJECT_SOURCE_DIR)
     else()
         include(FetchContent)
 
-        set(CANN_CMAKE_TAG "master-051")
+        set(CANN_CMAKE_TAG "9.2.0-001")
         if(CANN_3RD_LIB_PATH AND EXISTS "${CANN_3RD_LIB_PATH}/cmake-${CANN_CMAKE_TAG}.tar.gz")
             FetchContent_Declare(
                 cann-cmake
                 URL "${CANN_3RD_LIB_PATH}/cmake-${CANN_CMAKE_TAG}.tar.gz"
-                URL_HASH SHA256=f50ea0bf7f80ce9e664f26693252aee8d2a42438a80bc2cd335741dc4b3b834f
+                URL_HASH SHA256=d0709c53d6c89259a2cd229842c8ab92f5f39315d0ae1a1f53ef1a8b58bdb73f
             )
         else()
             FetchContent_Declare(
